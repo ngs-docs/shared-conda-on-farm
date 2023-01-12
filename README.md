6b806fd6cf1dce0d72505988ec822640759c9390
C. Titus Brown <titus@idyll.org>
2023-01-11 21:43:19 -0800

    upd

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # shared-conda-on-farm
+
 Scripts and config for setting up a shared conda package space on farm.cse.ucdavis.edu
+
+You'll want to do:
+```
+wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh
+```
+in this directory before running.
---
+++ install-miniforge.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+set -e
+
+cd ~/
+mkdir -p ~/.conda/pkgs
+cp ~ctbrown/shared-conda-on-farm/condarc ~/.condarc
+bash ~ctbrown/shared-conda-on-farm/Miniforge3-Linux-x86_64.sh -b -p $HOME/miniforge3
+
+# install!
+eval "$(~/miniforge3/bin/conda shell.bash hook)"
+conda init
+
+# add it to the default login on farm
+echo 'source .bashrc' > ~/.bash_profile
+
+# now, load conda
+source ~/.bashrc
+
+# install mamba
+conda install -y mamba
+
+# and also do mamba init!
+mamba init
+
+exit 0
